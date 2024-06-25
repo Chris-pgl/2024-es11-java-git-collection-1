55c45409bce79ee049516aee34a5a590eefdd0fb
Chris <cr1s@hotmail.it>
2024-06-25 16:46:26 +0200

    Finish Exercise

--- src/App.java
+++ src/App.java
@@ -1,3 +1,7 @@
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 public class App {
 
     /**
@@ -20,6 +24,44 @@ public class App {
      */
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+
+        testRandomInteger();
+        testRandomUniqueInteger();
+        testFrequencyMap();
+    }
+
+    public static void testRandomInteger(){
+        System.out.println("Test RandomInteger: ");
+        List<Integer> list1 = DataHelper.getRandomInteger(10);
+        System.out.println("Random list (0 - 10): " + list1);
+        DataHelper.getRandomInteger(10,10);
+        List<Integer> list2 = DataHelper.getRandomInteger(10, 20);
+        System.out.println("Random list2 (0 - 20): " + list2);
+        DataHelper.getRandomInteger(10,0,10);
+        List<Integer> list3 = DataHelper.getRandomInteger(10, 5, 15);
+        System.out.println("Random List3 (5 - 15): " + list3);
+    }
+
+    public static void testRandomUniqueInteger(){
+        System.out.println("Test RandomUniqueInteger:");
+        Set<Integer> set1 = DataHelper.getRandomUniqueInteger(10);
+        System.out.println("Unique Random: (0 - 10): " + set1);
+        Set<Integer> set2 = DataHelper.getRandomUniqueInteger(10,20);
+        System.out.println("Unique Random: (0 - 20): " + set2);
+        Set<Integer> set3 = DataHelper.getRandomUniqueInteger(10,5,15);
+        System.out.println("Unique Random: (5 - 15): " + set3);
+
+    }
+
+    public static void testFrequencyMap(){
+        System.out.println("Testin FrequencyMap:");
+        List<Integer> ram1 = DataHelper.getRandomInteger(25, 5, 15);
+        Map<Integer, Integer> f1 = DataHelper.getFrequencyMap(ram1);
+        System.out.println("RandomList: " + ram1);
+        System.out.println("FrequencyMap: " + f1);
+
+
+
     }
 }
--- src/DataHelper.java
+++ src/DataHelper.java
@@ -1,4 +1,8 @@
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.Set;
 
 public class DataHelper {
 
@@ -6,45 +10,55 @@ public class DataHelper {
 
     // restituisce una lista di numeri interi casuali compresi tra 0 e 10
     public static List<Integer> getRandomInteger(int size) {
-
-        return null;
+        return getRandomInteger(size,0,10);
     }
 
     // restituisce una lista di numeri interi casuali compresi tra 0 e max
     public static List<Integer> getRandomInteger(int size, int max) {
-
-        return null;
+        return getRandomInteger(size,0, max);
     }
 
     // restituisce una lista di numeri interi casuali compresi tra min e max
     public static List<Integer> getRandomInteger(int size, int min, int max) {
-
-        return null;
+        Random rmd = new Random();
+        List<Integer> list = new java.util.ArrayList<Integer>();
+        for (int i = 0; i < size; i++) {
+            list.add(rmd.nextInt(max - min + 1) + min);
+            }
+            return list;
     }
 
     // restituisce una lista di numeri casuali NON DUPLICATI compresi tra 0 e 10
-    // public static ??? getRandomUniqueInteger(int size) {
-    //
-    // return null;
-    // }
+     public static Set<Integer> getRandomUniqueInteger(int size) {
+        return getRandomUniqueInteger(size,0,10);
+     }
 
     // // restituisce una lista di numeri casuali NON DUPLICATI compresi tra 0 e max
-    // public static ??? getRandomUniqueInteger(int size, int max) {
-    //
-    // return null;
-    // }
+     public static Set<Integer> getRandomUniqueInteger(int size, int max) {
+        return getRandomUniqueInteger(size,0,max);
+     }
 
     // // restituisce una lista di numeri casuali NON DUPLICATI compresi tra min e
     // max
-    // public static ??? getRandomUniqueInteger(int size, int min, int max) {
-    //
-    // return null;
-    // }
+     public static Set<Integer> getRandomUniqueInteger(int size, int min, int max) {
+        Random rmd = new Random();
+        Set<Integer> set = new java.util.HashSet<>();
+        for (int i = 0; i < size; i++){
+            int num = rmd.nextInt(max - min + 1) + min;
+            while (set.contains(num)) {
+                num = rmd.nextInt(max - min + 1) + min;
+                }
+                set.add(num);
+        }
+        return set;
+    }
 
     // restituisce una mappa di frequenza di numeri interi
-    // public static ??? getFrequencyMap(List<Integer> list) {
-    //
-    // return null;
-    // }
-
+     public static Map<Integer, Integer> getFrequencyMap(List<Integer> list) {
+        Map<Integer, Integer> frequencyMap = new HashMap<>();
+        for(Integer number : list){
+            frequencyMap.put(number, frequencyMap.getOrDefault(number, 0) +1);
+        }
+        return frequencyMap;
+    }
 }
